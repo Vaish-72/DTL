3cbadb5ccdb1f8d48cf0aa7936312dd862729836
daryappa8018 <daryappamane098@gmail.com>
2025-04-15 15:05:04 +0530

    hello is added

--- hello.py
+++ hello.py
@@ -11,4 +11,4 @@ def from_hex(h):
 if __name__ == "__main__":
     print("Hex of 255:", to_hex(255))          # Output: 0xff
     print("Decimal of 0xff:", from_hex("0xff"))  # Output: 255
-    
+    print("Hex of 0x1A3:", to_hex(0x1A3))      # Output: 0x1a3
