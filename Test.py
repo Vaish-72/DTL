777282612fc362cccc2b9250e4debd703028b2b0
daryappa8018 <daryappamane098@gmail.com>
2025-04-15 14:09:47 +0530

    hello

--- Test.py
+++ Test.py
@@ -8,3 +8,5 @@ def from_hex(h):
 if __name__ == "__main__":
     print("Hex of 255:", to_hex(255))       # Prints 0xff
     print("Decimal of 0xff:", from_hex("0xff"))  # Prints 255
+
+    print("Hex of 0x1A3:", to_hex(0x1A3))  # Prints 0x1a3
